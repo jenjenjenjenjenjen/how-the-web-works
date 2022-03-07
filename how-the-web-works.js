03d8a5b464a8087a22ac77ce998abb1f650b386c
Jenny <jenny@Jennys-MacBook-Pro.local>
2022-03-06 18:14:05 -0700

    pushing to github

---
+++ how-the-web-works.js
@@ -0,0 +1,10 @@
+// HTTP is hypertext transfer protocol.
+// A URL is a universal resource locator.
+// DNS is a domain name system.
+// A query string is a part of the URL that provides additional information like search terms.
+// Two HTTP verbs are GET and POST and they are different in the way they behave.
+// GET processes requests without side effects, while POST processes requests with side effects.
+// An HTTP request is specified in a URL to access a resource on the server.
+// An HTTP response provides the resource specified in the HTTP request.
+// An HTTP header is used in a response to give more context to the content of the response.
+// Some headers include the date and time the server thinks it is, or any cookies the server wants to set.
